759bcf778cbf999023f23e13ae0bd37f6c96fafe
Bharanee Rathna <deepfryed@gmail.com>
2011-11-26 21:52:14 +1100

    reopen stdout, stderr when daemonized

--- lib/pastry.rb
+++ lib/pastry.rb
@@ -10,20 +10,29 @@ class Pastry
     @pool    = pool
     @app     = app
     @host    = options.fetch :host,       '127.0.0.1'
-    @port    = options.fetch :port,       3000
     @unix    = options.fetch :socket,     nil
-    @queue   = options.fetch :queue,      1024
     @logfile = options.fetch :logfile,    nil
     @daemon  = options.fetch :daemonize,  false
     @pidfile = options.fetch :pidfile,    '/tmp/pastry.pid'
-    @timeout = options.fetch :timeout,    30
-    @maxconn = options.fetch :maxconn,    1024
-    # TODO: validation
+    @name    = options.fetch :name,       nil
+
+    @port    = options.fetch(:port,    3000).to_i
+    @queue   = options.fetch(:queue,   1024).to_i
+    @maxconn = options.fetch(:maxconn, 1024).to_i
+    @timeout = options.fetch(:timeout,   30).to_i
   end
 
   def start
     ensure_not_running!
     Process.daemon if daemon
+
+    if daemon || logfile
+      STDOUT.reopen(logfile || '/tmp/pastry.log', 'a')
+      STDERR.reopen(logfile || '/tmp/pastry.log', 'a')
+      STDOUT.sync = true
+      STDERR.sync = true
+    end
+
     start!
   end
 
@@ -39,8 +48,12 @@ class Pastry
     File.open(pidfile, 'w') {|fh| fh.write(Process.pid)}
   end
 
+  def name
+    '%s master' % (@name || 'pastry')
+  end
+
   def motd
-    "starting pastry with #{pool} flakes listening on #{unix ? 'socket %s ' % unix : 'port %d' % port}"
+    "starting #{name} with #{pool} minions listening on #{unix ? 'socket %s ' % unix : 'port %d' % port}"
   end
 
   def start!
@@ -54,11 +67,12 @@ class Pastry
     server.extend(PastryServer)
 
     server.app = @app
-    logger     = Logger.new(logfile || daemon ? '/tmp/pastry.log' : $stdout, 0)
+    logger     = Logger.new(logfile || (daemon ? '/tmp/pastry.log' : $stdout), 0)
     options    = {timeout: @timeout, maximum_connections: @maxconn}
 
     logger.info motd
 
+    $0         = name if @name
     @running   = true
     pids       = pool.times.map {|n| run(server, options, n) }
 
@@ -82,13 +96,13 @@ class Pastry
       end
     end
 
-    at_exit { FileUtils.rm_f(pidfile) }
+    at_exit { FileUtils.rm_f(pidfile); FileUtils.rm_f(unix.to_s) }
     Process.waitall rescue nil
   end
 
   def run server, options, worker
     fork do
-      $0 = "|___ pastry chef #{worker} (started: #{Time.now})"
+      $0 = "#{@name ? "%s worker" % @name : "pastry chef"} #{worker} (started: #{Time.now})"
       EM.run { Backend.new(options).start(server) }
     end
   end
